8d9276eaa0ce82358c73aaa56194ea9de3bbb396
Ravi <thedevtool@gmail.com>
2023-08-07 23:24:26 +0530

    initial commit

---
+++ src/test/java/com/learning/cloud/springsecutiry/SpringSecutiryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.cloud.springsecutiry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecutiryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
